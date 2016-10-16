b548e6f92f38aca2236c3c7e2b27c8fee2b62ac5
Andrew Fountain <andrew@drup.org>
2016-10-15 21:56:34 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,7 @@ sudo -u www-data bash -c "git pull"
 sudo -u www-data bash -c "writeup index.txt"
 writeup build.txt -o build_4490570.php
 # Update Permissions by running this:
-#chown -R www-data .git *
+# chown -R www-data /home/test/rks
+# See also for github hooks:
+# https://gist.github.com/cowboy/619858
+# https://gist.github.com/phedoreanu/11321236
