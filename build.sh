5abb7f8d12ecaf8455cd8a826d844c6e9cf4a2ef
andrewfn <andrew@drup.org>
2016-11-18 00:04:44 -0500

    basic functioning legal pages

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 #build website
 sudo -u www-data bash -c "git pull"
 sudo -u www-data bash -c "writeup index.txt"
+sudo -u www-data bash -c "writeup law.txt"
 writeup build.txt -o build_4490570.php
 # Update Permissions by running this:
 # chown -R www-data /home/test/rks
